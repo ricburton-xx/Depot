db14ce5f426d9ce0dade975dd9f0adb7b8beb46f
Richard Burton <richard@getnudge.com>
2010-01-16 18:33:44 +0000

    Half way ajaxify

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @products = Product.find_products_for_sale
+    @cart = find_cart
     @count = increment_count
     @count_min_five = true if @count > 5
   end
@@ -18,6 +19,7 @@ class StoreController < ApplicationController
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
+    redirect_to_index
   rescue
     logger.error("I'm sorry you've tried to add a product that's not there #{params[:id]}")
     redirect_to_index("Invalid product")
@@ -33,8 +35,8 @@ class StoreController < ApplicationController
   end
 
 private
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => 'index'
   end
 
